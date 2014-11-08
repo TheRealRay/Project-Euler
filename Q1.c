cc06bdebbb5097352a6ac2364f8981ce4dfa24a8
Ray Chun <m5chun@uwaterloo.ca>
2014-11-08 17:01:50 -0500

    Update to a more efficient way and new algorithm

--- Q1.c
+++ Q1.c
@@ -1,4 +1,5 @@
 /* Calculate the sum of all multiples of 3 or 5 bellow 1000 */
+/* MY WAY
 #include <stdio.h>
 
 int main(void){
@@ -10,4 +11,23 @@ int main(void){
     }
     printf("%d\n", sum);
     return 0;
+}
+
+*/
+
+//More efficiently done-
+#include <stdio.h>
+
+int sumOfdivisible(int n);
+int main(void){
+    int max = 999;
+    printf("%d\n", (sumOfDivisible(3, max)+sumOfDivisible(5, max)-sumOfDivisible(15, max)));
+    
+    
+}
+int sumOfDivisible(int n, int target){
+    int max = target / n;
+    return (n*(max *(max+1))/2);
+    //Algorithm to get sum of all multiples of a number, up to a max
+    //n is the multiple, 1+2+3+4+...+max = (max+max+1)/2
 }
